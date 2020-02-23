b4454a39f494c6632733f14c39882ee4f3212e59
shivang <shivaang14@gmail.com>
2020-02-23 13:31:13 -0500

    Add registration process with checks

--- MandirTicket/__init__.py
+++ MandirTicket/__init__.py
@@ -2,6 +2,7 @@ import flask
 import urllib
 from flask import request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 
 params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=DESKTOP-KEO3N1T\SQLEXPRESS;DATABASE=AssetsDB")
 app = flask.Flask(__name__)
@@ -9,6 +10,10 @@ app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc:///?odbc_connect=%s" % params
 
 db = SQLAlchemy(app)
-db_main_user = db.Table('User', db.metadata, autoload=True, autoload_with=db.engine)
+# db_main_user = db.Table('User', db.metadata, autoload=True, autoload_with=db.engine)
+
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+main_user = Base.classes.User
 
 import MandirTicket.routes
--- MandirTicket/routes.py
+++ MandirTicket/routes.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify
-from MandirTicket import app, db, db_main_user
+from MandirTicket import app, db, main_user
 
 @app.route('/', methods=['GET'])
 def home():
@@ -9,10 +9,49 @@ def home():
 @app.route('/api/login', methods=['POST'])
 def login():
     error = None
-    if request.method == 'POST':
-        results = db.session.query(db_main_user).all()
+    try:
+        results = db.session.query(main_user).all()
         for r in results:
-            print (r)
-        resp = jsonify(success=True)
-        return resp
+            print (r.UserName)
+        return jsonify(success=True), 200
+    except:
+        return jsonify(success=False), 404
+    return jsonify(success=False), 404
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        data = request.json
+
+        # If data is empty.
+        # Also it might be empty if no json header is not used
+        if not data:
+            return jsonify({'message' : 'Data is empty!'})
+        
+        # TODO: Check for unique username and email
+        username_query_result = db.session.query(main_user).filter(main_user.UserName == data['UserName'])
+        if username_query_result:
+            return jsonify({'message' : 'Username you entered exist in database. Please enter another!'}), 404
+        
+        email_query_result = db.session.query(main_user).filter(main_user.EmailAddress == data['EmailAddress'])
+        if email_query_result:
+            return jsonify({'message' : 'Email you entered have already registered. Please enter another or request password change!'}), 404
+        # TODO: Admin Athentication for Registration
+
+        user_add = main_user(
+            UserName=data['UserName'],
+            Password=data['Password'],
+            EmailAddress=data['EmailAddress'],
+            FirstName=data['FirstName'],
+            LastName=data['LastName'],
+            PhoneNumber=data['PhoneNumber'],
+            RoleID=data['RoleID'],
+            Enabled=True,
+            CreateUserID=data['CreateUserID'],
+            ModifiedUserID=1)
+        db.session.add(user_add)
+        db.session.commit()
+        return jsonify(success=True), 200
     return jsonify(success=False)
